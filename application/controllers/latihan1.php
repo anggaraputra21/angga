93af22e15b4b1da911fb0ac0a3a001c3f841aca6
angga <iifan7480@gmail.com>
2023-10-16 20:53:02 +0700

    upload file contoh1

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>" ;
+        echo "Nama saya Singgih Anggara Putra
+            Saya tinggal di BSP
+            Olahraga yang saya sukai adalah Badminton
+    }
+}
